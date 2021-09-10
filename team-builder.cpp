a54903c093281802543e2fb020f3916a9862a718
Austin-Derrick <aderrickson@colum.edu>
2021-09-10 10:30:20 -0500

    created worker class that inherits from IWork, has name and position members. Has virtual methods for getting their name, getting their position, and doing work.

--- team-builder.cpp
+++ team-builder.cpp
@@ -22,6 +22,20 @@ interface IDevelop {
 	virtual void write_code() = 0;
 };
 
+class worker : public IWork {
+	string name;
+	string position;
+public:
+	worker() = delete;
+	worker(const char* n, const char* p) : name(n), position(p) {}
+	virtual ~worker() {}
+	virtual const char* get_name() override
+	{ return this->name.c_str(); }
+	virtual const char* get_position() override
+	{ return this->position.c_str(); }
+	virtual void do_work() override { cout << "Works" << endl; }
+};
+
 int main(int argc, const char* argv[]) 
 {
 	return 0;
