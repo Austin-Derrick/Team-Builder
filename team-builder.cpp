e7af87903633fc9265e0e21980c8016947bdff79
Austin-Derrick <aderrickson@colum.edu>
2021-09-10 10:21:24 -0500

    Added interfaces for worker class, manager class, and developer class to decouple their behavior from the class implementation.

--- team-builder.cpp
+++ team-builder.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
+#define interface struct
+interface IWork {
+	virtual const char* get_name() = 0;
+	virtual const char* get_position() = 0;
+	virtual void do_work() = 0;
+};
+
+interface IManage {
+	virtual const vector<unique_ptr<IWork>>& get_team() = 0;
+	virtual void manage_team() = 0;
+};
+
+interface IDevelop {
+	virtual void write_code() = 0;
+};
+
 int main(int argc, const char* argv[]) 
 {
 	return 0;
